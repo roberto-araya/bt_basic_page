a04e65d9ad650c0ff06f7525d667cae1b1647b0d
Roberto Araya <roberto.araya@btester.cl>
2020-09-01 16:16:40 -0400

    by roberto_araya: Remove panelizer and panels dependencies, update configurations and dependencies.

--- src/Config/ConfigBasicPageOverride.php
+++ src/Config/ConfigBasicPageOverride.php
@@ -11,90 +11,59 @@ use Drupal\Core\Config\ConfigFactory;
  */
 class ConfigBasicPageOverride implements ConfigFactoryOverrideInterface {
 
-  private $createContent;
-  private $deleteContent;
-  private $deleteOwnContent;
-  private $editContent;
+  /**
+   * View configuration object of bt_content.
+   *
+   * @var viewsAdminContent
+   */
   private $viewsAdminContent;
-  private $viewsFullAdminContent;
+
+  /**
+   * Editorial workflow configuration object.
+   *
+   * @var workflow
+   */
+  private $workflow;
 
   /**
    * {@inheritdoc}
    */
   public function __construct(ConfigFactory $configFactory) {
 
-    $this->createContent = $configFactory->get('user.role.bt_create_content');
-    $this->deleteContent = $configFactory->get('user.role.bt_delete_content');
-    $this->deleteOwnContent = $configFactory->get('user.role.bt_delete_own_content');
-    $this->editContent = $configFactory->get('user.role.bt_edit_publish_content');
-    $this->viewsAdminContent = $configFactory->get('views.view.bt_admin_content');
-    $this->viewsFullAdminContent = $configFactory->get('views.view.bt_full_admin_content');
+    $this->viewsAdminContent = $configFactory->get('views.view.bt_content');
+    $this->workflow = $configFactory->get('workflows.workflow.editorial');
   }
 
   /**
    * {@inheritdoc}
    */
   public function loadOverrides($names) {
-    $overrides = array();
+    $overrides = [];
 
-    // Add article permissions to bt_create_content role.
-    if (in_array('user.role.bt_create_content', $names)) {
-      $basic_page_permissions = [
-        'create bt_basic_page content',
-        'edit own bt_basic_page content',
-        'revert bt_basic_page revisions',
-        'view bt_basic_page revisions',
-      ];
-      $content_role = $this->createContent;
-      $permissions = array_merge($content_role->get('permissions'), $basic_page_permissions);
-      $overrides['user.role.bt_create_content']['permissions'] = $permissions;
-    }
-    // Add article permissions to bt_delete_content role.
-    if (in_array('user.role.bt_delete_content', $names)) {
-      $basic_page_permissions = [
-        'delete any bt_basic_page content',
-        'delete bt_basic_page revisions',
-      ];
-      $content_role = $this->deleteContent;
-      $permissions = array_merge($content_role->get('permissions'), $basic_page_permissions);
-      $overrides['user.role.bt_delete_content']['permissions'] = $permissions;
-    }
-    // Add article permissions to bt_delete_own_content role.
-    if (in_array('user.role.bt_delete_own_content', $names)) {
-      $basic_page_permissions = [
-        'delete own bt_basic_page content',
-      ];
-      $content_role = $this->deleteOwnContent;
-      $permissions = array_merge($content_role->get('permissions'), $basic_page_permissions);
-      $overrides['user.role.bt_delete_own_content']['permissions'] = $permissions;
-    }
-    // Add article permissions to bt_edit_publish_content role.
-    if (in_array('user.role.bt_edit_publish_content', $names)) {
-      $basic_page_permissions = [
-        'edit any bt_basic_page content',
-      ];
-      $content_role = $this->editContent;
-      $permissions = array_merge($content_role->get('permissions'), $basic_page_permissions);
-      $overrides['user.role.bt_edit_publish_content']['permissions'] = $permissions;
-    }
     $basic_page_values = [
       'bt_basic_page' => 'bt_basic_page',
     ];
-    // Add article filter values to views.view.bt_admin_content view.
-    if (in_array('views.view.bt_admin_content', $names)) {
+
+    // Add article filter values to views.view.bt_content view.
+    if (in_array('views.view.bt_content', $names)) {
       $views = $this->viewsAdminContent;
       $filter_values = $views->get('display.default.display_options.filters.type.value');
       $values = array_merge($filter_values, $basic_page_values);
-      $overrides['views.view.bt_admin_content']['display']['default']['display_options']['filters']['type']['value'] = $values;
-      $overrides['views.view.bt_admin_content']['display']['default']['display_options']['filters']['type_expose']['value'] = $values;
+      $overrides['views.view.bt_content']['display']['default']['display_options']['filters']['type']['value'] = $values;
     }
-    // Add article filter values to views.view.bt_full_admin_content view.
-    if (in_array('views.view.bt_full_admin_content', $names)) {
-      $views = $this->viewsFullAdminContent;
-      $filter_values = $views->get('display.default.display_options.filters.type.value');
-      $values = array_merge($filter_values, $basic_page_values);
-      $overrides['views.view.bt_full_admin_content']['display']['default']['display_options']['filters']['type']['value'] = $values;
-      $overrides['views.view.bt_full_admin_content']['display']['default']['display_options']['filters']['type_expose']['value'] = $values;
+
+    if (in_array('workflows.workflow.editorial', $names)) {
+      $workflow = $this->workflow;
+      $entity_types_values = $workflow->get('type_settings.entity_types');
+      if (array_key_exists('node', $entity_types_values)) {
+        $entity_types_values['node'][] = 'bt_basic_page';
+        $overrides['workflows.workflow.editorial']['type_settings']['entity_types']['node'] = $entity_types_values['node'];
+      }
+      else {
+        $values = ['bt_basic_page'];
+        $overrides['workflows.workflow.editorial']['type_settings']['entity_types']['node'] = $values;
+      }
+
     }
 
     return $overrides;
